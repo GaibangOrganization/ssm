37a79ff9c99c51defbbda509578d78a498bbabae
Administrator <Administrator@PC-20160707DHCW>
2017-05-18 20:08:40 +0800

    随机数改了

--- Random/src/com/belemo/test/RandonTest.java
+++ Random/src/com/belemo/test/RandonTest.java
@@ -1,11 +1,21 @@
 package com.belemo.test;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class RandonTest {
 	public static void main(String[] args) {
 		String[] persons = {"张三","李四","王五"};
-		System.out.println(Math.random()*10);
 		
+		
+		int r = (int)(Math.random()*3);
+		System.out.println(r);
+		if (r<persons.length) {
+			System.out.println(persons[r]);
+		} 
+		
+		
+			
 	}
+	
 }
