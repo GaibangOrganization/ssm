da8ce50b19dff206f6aa57f3a4e853106a5481f9
Administrator <Administrator@PC-20160707DHCW>
2017-05-18 17:02:32 +0800

    first上传

---
+++ Random/src/com/belemo/test/RandonTest.java
@@ -0,0 +1,11 @@
+package com.belemo.test;
+
+import java.util.Arrays;
+
+public class RandonTest {
+	public static void main(String[] args) {
+		String[] persons = {"张三","李四","王五"};
+		System.out.println(Math.random()*10);
+		
+	}
+}
